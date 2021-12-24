e35f2a9a72d9b0f8c7cac86b0229499bcaf48eb0
kanu27 <kanu.ponce@gmail.com>
2021-12-24 18:47:41 -0300

    finalizado el punto 3

--- main.py
+++ main.py
@@ -1,54 +1,86 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import re
+import json
 
 
 def getData():
     dataCsv = pd.read_csv(
         "data/country_vaccinations.csv",
-        usecols=["country","iso_code","date","total_vaccinations","people_vaccinated","people_fully_vaccinated","daily_vaccinations_raw","daily_vaccinations","total_vaccinations_per_hundred","people_vaccinated_per_hundred","people_fully_vaccinated_per_hundred","daily_vaccinations_per_million","vaccines"],
+        usecols=["country", "iso_code", "date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "daily_vaccinations_raw", "daily_vaccinations",
+                 "total_vaccinations_per_hundred", "people_vaccinated_per_hundred", "people_fully_vaccinated_per_hundred", "daily_vaccinations_per_million", "vaccines"],
         parse_dates=["date"],
-        na_values=[".", "???",""],
+        na_values=[".", "???", ""],
         sep=",",
         index_col="iso_code"
-        )
+    )
     dataCsv = dataCsv.fillna(0)
     dfCovid = pd.DataFrame(data=dataCsv)
     return dfCovid
 
+def personasVacunadasXPais():
+    # Cantidad personas vacunadas por pais
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    dataMundial = dfData[dfData["date"] == "2021-12-05"]
+    dfPaisesSort = dataMundial.sort_values("people_vaccinated", ascending=True)
+    ax1.bar(dfPaisesSort["country"], dfPaisesSort["people_vaccinated"])
+    plt.xticks(rotation=90)
+    plt.title("Cantidad de vacunas diarias en Chile", fontsize=20, color="b")
+    plt.xlabel('Pais', fontsize=14)
+    plt.ylabel('cantidad de vacunados', fontsize=14)
+
+
+def personasVacunadasEnChile():
+    # Cantidad personas vacunadas de chile por fecha
+    fig1 = plt.figure()
+    ax2 = fig1.add_subplot(1, 1, 1)
+    dataChile = dfData[dfData["country"] == "Chile"]
+    ax2.plot(dataChile["date"], dataChile["daily_vaccinations_raw"])
+    plt.title("Cantidad de vacunas diarias en Chile", fontsize=20, color="b")
+    plt.ylabel('cantidad de vacunados', fontsize=14)
+    plt.xlabel('Tiempo', fontsize=14)
+
+
+# 3 Mostrar porcentualmente y en bruto (en un mismo gráfico) el porcentaje/cantidad de tipos de vacuna (vaccines) (2.5 pt.)
+def porcentajeValor(pct, todosLosValores): 
+    absoluto = int(pct / 100*np.sum(todosLosValores))
+    return "{:.1f}%\n({:d})".format(pct, absoluto) 
+
+def TipoDeVacunas():
+    vacunasDict = dict()
+    vacunasList = list()
+    vacunasPorPais = dfData.groupby('country').agg('vaccines').unique()
+    for vacunas in vacunasPorPais:
+        vacunas = vacunas[0].replace(' ', '')
+        listaDeVacunasPorPais = vacunas.split(',')
+        for vacuna in listaDeVacunasPorPais:
+            vacunasList.append(vacuna)
+            vacunasDict[vacuna] = 0
+        for vacuna in vacunasList:
+            vacunasDict[vacuna] += 1
+    listaKeys = list()
+    listaData = list()
+    for key in vacunasDict:
+        listaKeys.append(key)
+        listaData.append(vacunasDict[key])
+    fig, ax = plt.subplots()
+    wedges, texts, autotexts = ax.pie(listaData,
+                                      autopct=lambda pct: porcentajeValor(pct, listaData),
+                                      labels=listaKeys,
+                                      shadow=True,
+                                      startangle=90,
+                                      textprops=dict(color="white"))
+
+    ax.legend(wedges, listaKeys,
+              title="Tipos de Vacunas",
+              loc="center left",
+              bbox_to_anchor=(1, 0, 0.5, 1))
+
+    plt.setp(autotexts, size=8, weight="bold")
+    ax.set_title("Vacunas presentes paises")
 
 dfData = getData()
-
-fig = plt.figure()
-
-"""ax1 = fig.add_subplot(2, 2, 1)
-ax2 = fig.add_subplot(2, 2, 2)
-#ax3 = fig.add_subplot(1, 1, 3)
-
-#Cantidad personas vacunadas por pais
-
-dataMundial = dfData[dfData["date"] == "2021-12-05"]
-dfPaisesSort = dataMundial.sort_values("people_vaccinated",ascending=True)
-ax1.bar(dfPaisesSort["country"],dfPaisesSort["people_vaccinated"])
-plt.title("Cantidad de vacunas diarias en Chile", fontsize=20, color="b")
-plt.xlabel('cantidad de vacunados', fontsize=14)
-plt.ylabel('Pais', fontsize=14)
-
-
-
-#Cantidad personas vacunadas de chile por fecha
-
-dataChile = dfData[dfData["country"] == "Chile"]
-ax2.plot(dataChile["date"],dataChile["daily_vaccinations_raw"])
-plt.title("Cantidad de vacunas diarias en Chile", fontsize=20, color="b")
-plt.ylabel('cantidad de vacunados', fontsize=14)
-plt.xlabel('Tiempo', fontsize=14)"""
-
-
-
-
-grouped = dfData.groupby('country').agg('vaccines')
-grouped = grouped.set_index(['country','vaccines'])
-second = grouped.join(dfData.set_index(['country', 'vaccines']))
-print(second)
+TipoDeVacunas()
 plt.show()
